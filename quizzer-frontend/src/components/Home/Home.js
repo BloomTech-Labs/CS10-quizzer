21a3fd6c56e116923fe0cef10e3888990a23afcf
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-09-11 17:44:45 -0400

    Fixed merge conflict for Home component

--- quizzer-frontend/src/components/Home/Home.js
+++ quizzer-frontend/src/components/Home/Home.js
@@ -8,7 +8,6 @@ import NavBar from '../NavBar/NavBar'
 import './Home.css'
 
 class Home extends Component {
-<<<<<<< HEAD
 
     render() {
         return (
@@ -36,23 +35,5 @@ class Home extends Component {
         )
     }
 };
-=======
-  render () {
-    return (
-      <div>
-        <NavBar />
-        <div className='main-content'>
-          <div className='main-content-left'>
-            <h1>A cool quiz creation app for teachers.</h1>
-            <p>Search millions of publicly created quizzes to improve your grades or create your own if you are a teacher.</p>
-            <Button color='info' className='getstart'>Get started</Button>
-          </div>
-          <img className='firstimg' src={firstimg} alt='Hero' />
-        </div>
-      </div>
-    )
-  }
-}
->>>>>>> 335c94d23378b4ed07a0205797a8a28db54b47b4
 
 export default Home
