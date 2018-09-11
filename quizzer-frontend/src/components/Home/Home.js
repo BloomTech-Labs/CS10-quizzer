54ad115ecc20014ba9d58b4bc0e171e7bfaf8087
Mephestys <mephestys@gmail.com>
2018-09-11 12:13:12 -0600

    Merged changes from `Edward-Gonzalez-Branch` and applied Standard JS.

--- quizzer-frontend/src/components/Home/Home.js
+++ quizzer-frontend/src/components/Home/Home.js
@@ -5,23 +5,6 @@ import NavBar from '../NavBar/NavBar'
 import './Home.css'
 
 class Home extends Component {
-<<<<<<< HEAD
-    render() {
-        return (
-            <div>
-                <NavBar />
-                <div className="main-content">
-                    <div className="main-content-left">
-                        <h1>A cool web app for teachers to create dynamic quizzes.</h1>
-                        <p>Search millions of publicly created quizzes to improve your grades as a student or create your own if you are a teacher.</p>
-                        <Button color="info" className="getstart">Get started</Button>
-                    </div>
-                    <img className="firstimg" src={firstimg} alt="Main Image" />
-                </div>
-            </div>
-        )
-    }
-=======
   render () {
     return (
       <div>
@@ -32,12 +15,11 @@ class Home extends Component {
             <p>Search millions of publicly created quizzes to improve your grades or create your own if you are a teacher.</p>
             <Button color='info' className='getstart'>Get started</Button>
           </div>
-          <img className='firstimg' src={firstimg} alt='Main' />
+          <img className='firstimg' src={firstimg} alt='Hero' />
         </div>
       </div>
     )
   }
->>>>>>> e9b2305907eb6f4a0b9df9fe6b880c666e97c2c8
 }
 
 export default Home
--- quizzer-frontend/src/components/NavBar/NavBar.js
+++ quizzer-frontend/src/components/NavBar/NavBar.js
@@ -5,43 +5,23 @@ import { Button } from 'reactstrap'
 import './NavBar.css'
 
 class NavBar extends Component {
-<<<<<<< HEAD
-    render() {
-        return (
-            <div className='nav_container'>
-                <div className="nav_container_left">
-                    <Link className="nav_container_left_logo_button" to="/"><h1>Quizzer</h1></Link>
-                    <Button className="nav_container_left_createquiz_button">
-                        <span className="createquiz_button_plussign">&#x2795;</span>
-                        <span className="createquiz_button_text">Create Quiz</span>
-                    </Button>
-                </div>
-                <div className="nav_container_right">
-                    <Button className="nav_container_right_login_button">Log in</Button>
-                    <Button className="nav_container_right_signup_button" color="info">Sign up</Button>
-                </div>
-            </div>
-        )
-    }
-=======
   render () {
     return (
-      <div className='nav-container'>
-        <div className='home-group'>
-          <Link className='home-button' to='/'><h1>Quizzer</h1></Link>
-          <Button className='create-quiz'>
-            <span className='plus' role='img' aria-label='Create Quiz'>&#x2795;</span>
-            <span className='text'>Create Quiz</span>
+      <div className='nav_container'>
+        <div className='nav_container_left'>
+          <Link className='nav_container_left_logo_button' to='/'><h1>Quizzer</h1></Link>
+          <Button className='nav_container_left_createquiz_button'>
+            <span className='createquiz_button_plussign' role='img' aria-label='Create Quiz'>&#x2795;</span>
+            <span className='createquiz_button_text'>Create Quiz</span>
           </Button>
         </div>
-        <div className='button-group'>
-          <Button className='login'>Log in</Button>
-          <Button className='signup' color='info'>Sign up</Button>
+        <div className='nav_container_right'>
+          <Button className='nav_container_right_login_button'>Log in</Button>
+          <Button className='nav_container_right_signup_button' color='info'>Sign up</Button>
         </div>
       </div>
     )
   }
->>>>>>> e9b2305907eb6f4a0b9df9fe6b880c666e97c2c8
 }
 
 export default NavBar
