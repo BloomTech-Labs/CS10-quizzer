b52583597fec76ef8a7a57105d3422999237c9d5
bbenefield89 <bsquared18@gmail.com>
2018-09-11 17:39:52 -0400

    MOVE QUERY CLASSES TO THEIR OWN FOLDER
    - in quizzes/graphql/query.py
    This separation of concerns will make it easier to read and add new queries later down the road

--- server/quizzes/schema.py
+++ server/quizzes/schema.py
@@ -1,38 +1,8 @@
 from graphene_django import DjangoObjectType
 import graphene
+
+from quizzes.graphql.query import ClassType, QuizType, QuestionType, ChoiceType
 from quizzes.models import Class, Quiz, Question, Choice
-
-
-class ClassType(DjangoObjectType):
-    '''
-    ClassType GraphQL Schema
-    '''
-    class Meta:
-        model = Class
-
-
-class QuizType(DjangoObjectType):
-    '''
-    QuizType GraphQL Schema
-    '''
-    class Meta:
-        model = Quiz
-
-
-class QuestionType(DjangoObjectType):
-    '''
-    Question GraphQL Schema
-    '''
-    class Meta:
-        model = Question
-
-
-class ChoiceType(DjangoObjectType):
-    '''
-    ChoiceType GraphQL Schema
-    '''
-    class Meta:
-        model = Choice
 
 
 class CreateClass(graphene.Mutation):
