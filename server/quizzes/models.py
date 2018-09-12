bc35ecf00964ca2a213087ec08fed14711960cea
bbenefield89 <bsquared18@gmail.com>
2018-09-12 18:32:36 -0400

    REMOVE `ClassID` FROM `Teacher` & UPDATE MUTATIONS
    - in quizzes/models.py
    - in quizzes/graphql/mutation.py

--- server/quizzes/models.py
+++ server/quizzes/models.py
@@ -53,8 +53,6 @@ class Choice(models.Model):
 class Teacher(models.Model):
     TeacherID = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     TeacherName = models.CharField(max_length=50, blank=False)
-    # ClassID = models.ForeignKey('Class', on_delete=models.CASCADE)
-    ClassID = models.TextField(blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
