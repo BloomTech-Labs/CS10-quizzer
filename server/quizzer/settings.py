e70ee9e594de3b04047438483abb4e4736c13cc9
LordOrbnauticus <haywood.d.johnson@gmail.com>
2018-09-12 13:40:19 -0500

    added to pipenv; clear working tree

--- server/quizzer/settings.py
+++ server/quizzer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast = bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'https://cs10-quizzer.herokuapp.com/']
 
 
 # Application definition
@@ -125,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
