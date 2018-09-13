e8b4aef1a623d9d092e87f6a261b54dde822dae3
bbenefield89 <bsquared18@gmail.com>
2018-09-13 15:33:06 -0400

    ADD EMAIL + PW

--- quizzes/graphql/mutation.py
+++ quizzes/graphql/mutation.py
@@ -1,9 +1,12 @@
+
 import graphene
+import hashlib
 import jwt
 import time
 
 from decouple import config
 from quizzes.models import Class, Quiz, Question, Choice, Teacher, Student
+from uuid import uuid4
 
 
 '''
@@ -15,14 +18,12 @@ class CreateClass(graphene.Mutation):
     are required when making a mutation/POST request.
     
     The `ok` prop is required by GraphQL. This is not a Django Attribute
-
     The `new_class` prop is calling the `ClassMutation` class right below
     our `CreateClass` class. We then call the `mutate()` method, denoted with
     a `@staticmethod` decorator, that will then use the value of
     `ClassMutation.ClassName` and pass that to the `Class.objects.create()`
     method saved on the `new_class` variable inside of the
     `mutate()` method below
-
     EXAMPLE GRAPHQL MUTATION
     ------------------------
     
@@ -34,7 +35,6 @@ class CreateClass(graphene.Mutation):
             ok
         }
     }
-
     '''
     class Arguments:
         ClassName  = graphene.String()
@@ -50,7 +50,7 @@ class CreateClass(graphene.Mutation):
 
         # `user` variable needs to be changed to use the ID given by the JWT
         # for now JWT does NOT return a userID
-        user      = Teacher.objects.get(TeacherName=dec_jwt[ 'sub' ])
+        user = Teacher.objects.get(TeacherName=dec_jwt[ 'sub' ])
         
         # if token is expired return expiration error to user
         if dec_jwt[ 'exp' ] < time.time():
@@ -60,8 +60,8 @@ class CreateClass(graphene.Mutation):
         if not user:
             return print('\n\nUSER DOES NOT EXISTS\nRETURN ERRO TO CLIENT\n\n')
         
-        else:
-            new_class = Class.objects.create(ClassName=ClassName)
+        # this portion is unreachable if any of the above conditions are true
+        new_class = Class.objects.create(ClassName=ClassName)
 
         return CreateClass(new_class=new_class)
 
@@ -79,13 +79,16 @@ start CreateTeacher
 '''
 class CreateTeacher(graphene.Mutation):
     class Arguments:
-        TeacherName = graphene.String()
+        TeacherName  = graphene.String()
+        TeacherPW    = graphene.String()
+        TeacherEmail = graphene.String()
 
     jwt_string = graphene.String()
     teacher    = graphene.Field(lambda: TeacherMutation)
 
     @staticmethod
-    def mutate(self, info, TeacherName):
+    def mutate(self, info, TeacherName, TeacherPW, TeacherEmail):
+        # create DATA for JWT
         secret    = config('SECRET_KEY')
         algorithm = 'HS256'
         payload = {
@@ -94,16 +97,30 @@ class CreateTeacher(graphene.Mutation):
             'exp': time.time() + 86400
         }
 
-        enc_jwt = jwt.encode(payload, secret, algorithm=algorithm)
-
+        # create JWT as a byte string e.g. b'<< JWT >>'
+        enc_jwt    = jwt.encode(payload, secret, algorithm=algorithm)
+        # transforms JWT-byte-string into a normal UTF-8 string
         jwt_string = enc_jwt.decode('utf-8')
-        teacher = Teacher.objects.create(TeacherName=TeacherName)
 
+        # password hashing
+        # turn strings into byte-strings
+        password  = TeacherPW.encode('utf-8')
+        salt      = uuid4().hex.encode('utf-8')
+        pass_salt = b''.join([ password, salt ])
+        # hashlib.sha256 requires byte-strings in order to apply the algorithm
+        hashed_pw = hashlib.sha256(pass_salt).hexdigest()
+
+        # saving new Teacher into DB
+        teacher = Teacher.objects.create(TeacherName=TeacherName, TeacherPW=hashed_pw, TeacherEmail=TeacherEmail)
+
+        # this is what GraphQL is going to return
         return CreateTeacher(teacher=teacher, jwt_string=jwt_string)
 
 
 class TeacherMutation(graphene.ObjectType):
-    TeacherName = graphene.String()
+    TeacherName  = graphene.String()
+    TeacherPW    = graphene.String()
+    TeacherEmail = graphene.String()
         
 '''
 end CreateTeacher
