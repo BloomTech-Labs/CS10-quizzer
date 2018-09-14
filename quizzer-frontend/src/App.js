21c67feb26cac7150b6f4444949af9fe9f4c7505
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-09-14 01:38:39 -0400

    Commented out Quiz Page component until react router is implemented

--- quizzer-frontend/src/App.js
+++ quizzer-frontend/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-// import Home from './components/Home/Home'
-import QuizPage from './components/QuizPage/QuizPage'
+import Home from './components/Home/Home'
+// import QuizPage from './components/QuizPage/QuizPage'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div>
-        {/* <Home /> */}
-        <QuizPage />
+        <Home />
+        {/* <QuizPage /> */}
       </div>
     )
   }
