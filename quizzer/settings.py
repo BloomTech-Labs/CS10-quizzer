8f7322e129c2ca049848195aab10b936b1bd5188
bbenefield89 <bsquared18@gmail.com>
2018-09-13 21:39:44 -0400

    DEPLOY

--- quizzer/settings.py
+++ quizzer/settings.py
@@ -131,8 +131,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Graphene Django (GraphQL)
