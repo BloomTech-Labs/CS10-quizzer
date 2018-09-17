dbca6c0bca1b78d3ae3a0801dd01657052f9c9fe
bbenefield89 <bsquared18@gmail.com>
2018-09-17 12:24:24 -0400

    FORMAT VARIABLES
    - in quizzes/graphql/mutations.py

--- quizzes/models.py
+++ quizzes/models.py
@@ -4,74 +4,74 @@ from uuid import uuid4
 # Create your models here.
 
 class Class(models.Model):
-    ClassesID = models.UUIDField(primary_key=True, default = uuid4, editable = False)
-    ClassName = models.CharField(max_length = 50, blank = False)
-    created_at = models.DateTimeField(auto_now_add = True)
+    ClassID       = models.UUIDField(primary_key=True, default = uuid4, editable = False)
+    ClassName     = models.CharField(max_length = 50, blank = False)
+    created_at    = models.DateTimeField(auto_now_add = True)
     last_modified = models.DateTimeField(auto_now = True)
 
     class Meta:
-        db_table = 'Classes'
+        db_table            = 'Classes'
         verbose_name_plural = 'classes'
 
 class Quiz(models.Model):
-    QuizID = models.UUIDField(primary_key=True, default = uuid4, editable = False)
-    ClassesID = models.ForeignKey('Class', on_delete = models.CASCADE)
-    QuizName = models.CharField(max_length = 100, blank = False)
-    Public = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add = True)
+    QuizID        = models.UUIDField(primary_key=True, default = uuid4, editable = False)
+    ClassID       = models.ForeignKey('Class', on_delete = models.CASCADE)
+    QuizName      = models.CharField(max_length = 100, blank = False)
+    Public        = models.BooleanField(default=True)
+    created_at    = models.DateTimeField(auto_now_add = True)
     last_modified = models.DateTimeField(auto_now = True)
 
     class Meta:
-        db_table = 'Quizzes'
+        db_table            = 'Quizzes'
         verbose_name_plural = 'quizzes'
 
 class Question(models.Model):
-    QuestionID = models.UUIDField(primary_key=True, default = uuid4, editable = False)
-    QuizID = models.ForeignKey('Quiz', on_delete = models.CASCADE)
-    Question = models.TextField(blank = False)
-    created_at = models.DateTimeField(auto_now_add = True)
+    QuestionID    = models.UUIDField(primary_key=True, default = uuid4, editable = False)
+    QuizID        = models.ForeignKey('Quiz', on_delete = models.CASCADE)
+    Question      = models.TextField(blank = False)
+    created_at    = models.DateTimeField(auto_now_add = True)
     last_modified = models.DateTimeField(auto_now = True)
 
     class Meta:
-        db_table = 'Questions'
+        db_table            = 'Questions'
         verbose_name_plural = 'questions'
     
 
 class Choice(models.Model):
-    ChoiceID = models.UUIDField(primary_key=True, default = uuid4, editable = False)
-    QuestionID = models.ForeignKey('Question', on_delete = models.CASCADE)
-    Choice = models.TextField(blank = False)
-    isCorrect = models.BooleanField()
-    created_at = models.DateTimeField(auto_now_add = True)
+    ChoiceID      = models.UUIDField(primary_key=True, default = uuid4, editable = False)
+    QuestionID    = models.ForeignKey('Question', on_delete = models.CASCADE)
+    Choice        = models.TextField(blank = False)
+    isCorrect     = models.BooleanField()
+    created_at    = models.DateTimeField(auto_now_add = True)
     last_modified = models.DateTimeField(auto_now = True)
 
     class Meta:
-        db_table = 'Choices'
+        db_table            = 'Choices'
         verbose_name_plural = 'choices'
 
 
 class Teacher(models.Model):
-    TeacherID = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    TeacherEmail = models.CharField(max_length=256, unique=True, blank=False)
-    TeacherName = models.CharField(max_length=50, blank=False)
-    TeacherPW = models.CharField(max_length=256, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    TeacherID     = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    TeacherEmail  = models.CharField(max_length=256, unique=True, blank=False)
+    TeacherName   = models.CharField(max_length=50, blank=False)
+    TeacherPW     = models.CharField(max_length=256, blank=False)
+    created_at    = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'Teachers'
+        db_table            = 'Teachers'
         verbose_name_plural = 'teachers'
 
 
 class Student(models.Model):
-    StudentID = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    StudentName = models.CharField(max_length=50, blank=False)
-    StudentEmail = models.CharField(max_length=256, unique=True, blank=False)
-    ClassID = models.ForeignKey('Class', on_delete=models.CASCADE)
-    TeacherID = models.ForeignKey('Teacher', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    StudentID     = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    StudentName   = models.CharField(max_length=50, blank=False)
+    StudentEmail  = models.CharField(max_length=256, unique=True, blank=False)
+    ClassID       = models.ForeignKey('Class', on_delete=models.CASCADE)
+    TeacherID     = models.ForeignKey('Teacher', on_delete=models.CASCADE)
+    created_at    = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = 'Students'
+        db_table            = 'Students'
         verbose_name_plural = 'students'
