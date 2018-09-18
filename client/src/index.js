c092e87b2eb6d707f32060cafa55fa7e6e0d1ad3
Mephestys <mephestys@gmail.com>
2018-09-18 15:56:48 -0600

    Added `Authorization` header to all network requests in index.js - This can carry the JWT from local storage for server to verify user - Added new dependency: `apollo-link-context` (for auth headers) - Also updated node version to 8.12 in both package.json

--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,30 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import registerServiceWorker from './registerServiceWorker'
 import { ApolloProvider } from 'react-apollo'
-import ApolloClient from 'apollo-boost'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const apiURI = process.env.NODE_ENV !== 'production' ? 'http://localhost:8000/graphiql/' : '/graphiql/'
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: apiURI
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = window.localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 })
 
 render(
