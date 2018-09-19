4611b25e3ec9731862864f5aadb8badaf0d25a83
bbenefield89 <bsquared18@gmail.com>
2018-09-19 17:46:49 -0400

    ADD NEW MUTATION AND QUERIES
    - in quizzes/schema.py
      - class_quizzes
        - returns all Quizzes for a specific class
      - teacher_quizzes
        - returns all Quizzes for a specific Teacher
    
      - Mutations
        - create_question
          - creates a new Question

--- quizzes/schema.py
+++ quizzes/schema.py
@@ -6,9 +6,12 @@ import graphql
 from decouple import config
 from graphene_django import DjangoObjectType
 from graphql import GraphQLError, GraphQLObjectType, GraphQLList
-from quizzes.graphql.mutation import CreateTeacher, QueryTeacher, CreateStudent, CreateQuiz
 from quizzes.graphql.mutations.classes import CreateClass
 from quizzes.models import Class, Quiz, Question, Choice, Teacher, Student, Student_Quiz, Class_Quiz
+
+from quizzes.graphql.mutation import (
+    CreateTeacher, QueryTeacher, CreateStudent, CreateQuiz, CreateQuestion
+)
 
 from quizzes.graphql.query import (
     ClassType, QuizType, QuestionType, ChoiceType, TeacherType, StudentType, Class_QuizType
@@ -21,24 +24,27 @@ class Mutation(graphene.ObjectType):
     Mutation class allows us to make POST/Mutation requests to create new
     fields
     '''
-    create_class   = CreateClass.Field()
-    create_teacher = CreateTeacher.Field()
-    query_teacher  = QueryTeacher.Field()
-    create_student = CreateStudent.Field()
-    create_quiz    = CreateQuiz.Field()
+    create_class    = CreateClass.Field()
+    create_teacher  = CreateTeacher.Field()
+    query_teacher   = QueryTeacher.Field()
+    create_student  = CreateStudent.Field()
+    create_quiz     = CreateQuiz.Field()
+    create_question = CreateQuestion.Field()
 
 
 class Query(graphene.ObjectType):
     '''
     Allows us to make GET/Query requests from the DB using GraphQL
     '''
-    classes          = graphene.List(ClassType, enc_jwt=graphene.String())
-    public_quizzes   = graphene.List(QuizType)
-    questions        = graphene.List(QuestionType)
-    choices          = graphene.List(ChoiceType)
-    teachers         = graphene.List(TeacherType)
-    students         = graphene.List(StudentType)
-    class_quizzes    = graphene.List(Class_QuizType, ClassID=graphene.String())
+    classes         = graphene.List(ClassType, enc_jwt=graphene.String())
+    public_quizzes  = graphene.List(QuizType)
+    class_quizzes   = graphene.List(QuizType, ClassID=graphene.String())
+    teacher_quizzes = graphene.List(QuizType, enc_jwt=graphene.String())
+    questions       = graphene.List(QuestionType)
+    choices         = graphene.List(ChoiceType)
+    teachers        = graphene.List(TeacherType)
+    students        = graphene.List(StudentType)
+    # class_quizzes    = graphene.List(Class_QuizType, ClassID=graphene.String())
 
     teacher = graphene.Field(
         TeacherType,
@@ -75,15 +81,40 @@ class Query(graphene.ObjectType):
             << tableName >>__<< field >>__contains = << value to search for >>
             NOTE: these are double underscores between each field
             '''
-            return Class.objects.filter(
-                TeacherID__TeacherEmail__contains=teacher.TeacherEmail
-                )
+
+            return teacher.class_set.all()
+            
+            # return Class.objects.filter(
+            #     TeacherID__TeacherEmail__contains=teacher.TeacherEmail
+            #     )
 
         except:
             raise GraphQLError('Please supply a valid JWT')
 
     def resolve_public_quizzes(self, info):
         return Quiz.objects.filter(Public=True)
+
+    def resolve_class_quizzes(self, info, **kwargs):
+        class_id = kwargs.get('ClassID')
+
+        if class_id:
+            classroom = Class.objects.get(ClassID=class_id)
+            return classroom.quiz_set.all()
+
+        return GraphQLError('Please supply a valid ClassID')
+
+    def resolve_teacher_quizzes(self, info, **kwargs):
+        if 'enc_jwt' in kwargs:
+            enc_jwt   = kwargs.get('enc_jwt').encode('utf-8')
+            secret    = config('SECRET_KEY')
+            algorithm = 'HS256'
+            dec_jwt   = jwt.decode(enc_jwt, secret, algorithms=[ algorithm ])
+            teacherID = dec_jwt['sub']['id']
+            teacher   = Teacher.objects.get(TeacherID=teacherID)
+
+            return teacher.quiz_set.all()
+
+        return GraphQLError('Please supply a valid JWT')
 
     def resolve_questions(self, info):
         return Question.objects.all()
@@ -137,14 +168,6 @@ class Query(graphene.ObjectType):
 
     def resolve_students(self, info):
         return Student.objects.all()
-
-    def resolve_class_quizzes(self, info, **kwargs):
-        if 'ClassID' not in kwargs:
-            return GraphQLError('Please supply a valid ClassID')
-
-        class_id = kwargs.get('ClassID')
-
-        return Class_Quiz.objects.filter(ClassID=class_id)
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
