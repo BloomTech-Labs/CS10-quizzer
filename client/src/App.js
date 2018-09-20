19a0338bcdf7bfc7f908e99d01a6edb4591fe0fc
Mephestys <mephestys@gmail.com>
2018-09-20 15:10:40 -0600

    Basic settings structure laid out (no styling). - Added /settings/ route to App (will likely be overwritten by master) - Removed trailing comma in package.json

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Home from './components/Home/Home'
 import QuizPage from './components/QuizPage/QuizPage'
 import Result from './components/Result/Result'
+import Settings from './components/Settings/Settings'
 import { Route, withRouter } from 'react-router-dom'
 import './App.css'
 
@@ -12,6 +13,7 @@ class App extends Component {
         <Route exact path='/' component={Home} />
         <Route exact path='/quiz' component={QuizPage} />
         <Route exact path='/result' component={Result} />
+        <Route exact path='/settings' component={Settings} />
       </div>
     )
   }
--- client/src/components/Settings/Settings.js
+++ client/src/components/Settings/Settings.js
@@ -1,4 +1,49 @@
 /* This page allows a user to change the following:
   Email address
   Password
+
+  Should also import the Rocketlist component to provide navigation.
 */
+
+import React, { Component } from 'react'
+// import gql from 'graphql-tag'
+import { Button } from 'reactstrap'
+// import Rocketlist
+
+// GraphQL structures go here. We need to retrieve the current
+// email address and password (for the old password field), and we
+// need a mutation to request a change through the server.
+
+class Settings extends Component {
+  constructor () {
+    super()
+    this.state = {
+      email: '',
+      oldPassword: '',
+      newPassword: ''
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange (event) {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render () {
+    const { email, oldPassword, newPassword } = this.state
+    return (
+      <div className='settings_container'>
+        <span>Email:</span>
+        <input type='email' name='email' value={email} onChange={this.handleInputChange} />
+        <span>Old Password:</span>
+        <input type='password' name='oldPassword' value={oldPassword} onChange={this.handleInputChange} />
+        <span>New Password:</span>
+        <input type='password' name='newPassword' value={newPassword} onChange={this.handleInputChange} />
+        <Button type='submit' color='info' className='settings_save_button'>Save</Button>
+      </div>
+    )
+  }
+}
+
+export default Settings
