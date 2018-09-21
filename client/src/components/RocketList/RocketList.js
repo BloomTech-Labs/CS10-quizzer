cf1c5d6453a7cf40223987a0b6dc1e9a1cb16489
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-09-21 15:48:09 -0400

    Added welcome text for the user on the rocket list page

--- client/src/components/RocketList/RocketList.js
+++ client/src/components/RocketList/RocketList.js
@@ -4,16 +4,24 @@ import { Route, Link, withRouter } from 'react-router-dom'
 import './RocketList.css'
 
 class RocketList extends Component {
+  constructor () {
+    super()
+    this.state = {
+      username: ''
+    }
+  }
+
   render () {
     return (
       <div className='rocket_list_container'>
+        <span className='username'>Welcome {this.state.username}</span>
         <div className='rocket_list_navbar'>
           <Link to='/quizzes'>Quizzes</Link>
           <Link to='/classes'>Classes</Link>
           <Link to='/billing'>Billing</Link>
           <Link to='/settings'>Settings</Link>
         </div>
-        <Route exact path='/:userId/' Component={Home} />
+        <Route exact path='/:userId' Component={Home} />
       </div>
     )
   }
