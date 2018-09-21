83fdee7fa6373afdc9a1f5a6c43aa84a363edefa
Mephestys <mephestys@gmail.com>
2018-09-21 11:01:45 -0600

    Integrating new rocket list with Settings page - App.js Moved RocketList component to top of routes (now always displays first) - App.css Primary app page now Flexbox - Rocketlist.css Slight dimension changes to accomodate loaded pages -- This likely needs some adjusting. - Settings.css flex-direction changed to flex-flow: column nowrap

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,18 @@ import Result from './components/Result/Result'
 import Settings from './components/Settings/Settings'
 import RocketList from './components/RocketList/RocketList'
 import { Route, withRouter } from 'react-router-dom'
+import { Button } from 'reactstrap'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className='app'>
+        {localStorage.getItem('token') && localStorage.getItem('id') ? <Route exact path='/:userId' component={RocketList} /> : null}
         <Route exact path='/' component={Landing} />
         <Route exact path='/quiz' component={QuizPage} />
         <Route exact path='/result' component={Result} />
         <Route exact path='/settings' component={Settings} />
-        {localStorage.getItem('token') && localStorage.getItem('id') ? <Route exact path='/:userId' component={RocketList} /> : null}
       </div>
     )
   }
