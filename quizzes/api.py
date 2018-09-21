01d53b7f1b6804a0ee1469cfc997c21723c73f51
bbenefield89 <bsquared18@gmail.com>
2018-09-21 18:19:16 -0400

    FIX CONDITION TO USE == INSTEAD OF KEYWORD is
    - in quizzes/api.py

--- quizzes/api.py
+++ quizzes/api.py
@@ -4,9 +4,17 @@ from decouple import config
 from django.http import JsonResponse
 
 def make_payments(req):
-    print(req.method)
-    
-    if req.method is 'POST':
+    '''
+    TODO: prevent requests that are not authenticated from making transactions
+          this can be accomplished by checking req.body. From there we will
+          most likely want to check the JWT or some header that we can send
+          from the client
+
+    TODO: dynamically set: amount, recept_email
+
+    TODO: find out how to set currency depending on the users location
+    '''
+    if req.method == 'POST':
         # sets the stripe API key
         stripe.api_key = config('STRIPE_SECRET_KEY')
 
