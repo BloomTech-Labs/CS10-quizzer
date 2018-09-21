ea17fcd00b3bd31c7e1473942bfe92390d3eae87
bbenefield89 <bsquared18@gmail.com>
2018-09-21 15:44:55 -0400

    NEW ROUTE FOR <Stripe />
    - in App.js
      - this new component handles the heavy lifting of configuring our app to make payments
      - check component comments in code for documentation

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Home from './components/Home/Home'
 import QuizPage from './components/QuizPage/QuizPage'
 import Result from './components/Result/Result'
+import Stripe from './components/Stripe/Stripe'
 import { Route, withRouter } from 'react-router-dom'
 import './App.css'
 
@@ -12,6 +13,7 @@ class App extends Component {
         <Route exact path='/' component={Home} />
         <Route exact path='/quiz' component={QuizPage} />
         <Route exact path='/result' component={Result} />
+        <Route path='/stripe' component={Stripe} />
       </div>
     )
   }
