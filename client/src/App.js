1d7675d9a3ba3482f1de0f7fcffe7db28c53a630
bbenefield89 <bsquared18@gmail.com>
2018-09-24 16:26:46 -0400

    REMOVE Stripe IMPORT
    - in client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import Landing from './components/Landing/Landing'
 import RocketList from './components/RocketList/RocketList'
 import { Route, Redirect, withRouter } from 'react-router-dom'
-import Stripe from './components/Stripe/Stripe'
 import './App.css'
 
 class App extends Component {
@@ -13,7 +12,6 @@ class App extends Component {
         {localStorage.getItem('token') && localStorage.getItem('id') ? <Route exact path='/:userId' component={Landing} /> : null}
         {localStorage.getItem('token') && localStorage.getItem('id') ? <Route exact path='/:userId/:choice' component={RocketList} /> : null}
         {localStorage.getItem('token') && localStorage.getItem('id') && window.location.pathname === '/' ? <Redirect to={`/${localStorage.getItem('id')}`} /> : null}
-        {!localStorage.getItem('token') && !localStorage.getItem('id') ? <Route exact path='/' component={Stripe} /> : null }
       </div>
     )
   }
