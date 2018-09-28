571bddc15c9786857d7768790867ee157436d21c
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-09-28 12:45:43 -0400

    Fixed delete questions bug.

--- client/src/components/CreateQuiz/CreateQuiz.js
+++ client/src/components/CreateQuiz/CreateQuiz.js
@@ -22,11 +22,7 @@ class CreateQuiz extends Component {
   addQuestion = () => {
     const questionsArr = Object.assign([], this.state.questions)
     const choicesArr = Object.assign([], this.state.choices)
-    if (questionsArr.length === 0 && choicesArr.length === 0) {
-      questionsArr.push({ 'num': 0, 'question': '' })
-    } else {
-      questionsArr.push({ 'num': questionsArr[questionsArr.length - 1]['num'] + 1, 'question': '' })
-    }
+    questionsArr.push('')
     choicesArr.push({ '1': '', '2': '', '3': ['', true], '4': ['', true] })
     this.setState({
       questions: questionsArr,
@@ -37,7 +33,7 @@ class CreateQuiz extends Component {
   questionChange = (event) => {
     const questionsArr = Object.assign([], this.state.questions)
     const index = Number(event.target.name)
-    questionsArr[index].question = event.target.value
+    questionsArr[index] = event.target.value
     this.setState({
       questions: questionsArr
     })
@@ -47,9 +43,6 @@ class CreateQuiz extends Component {
     const questionsArr = Object.assign([], this.state.questions)
     const choicesArr = Object.assign([], this.state.choices)
     const index = Number(event.target.name)
-    if (questionsArr.length > 1 && index !== questionsArr.length - 1) {
-      questionsArr[index + 1].num = index
-    }
     questionsArr.splice(index, 1)
     choicesArr.splice(index, 1)
     this.setState({
@@ -71,10 +64,10 @@ class CreateQuiz extends Component {
         choices per question, but you are limited to four answer choices.</h4>
         <input className='create_quiz_name' name='quizName' onChange={this.handleOnChange} placeholder='Name' type='text' required />
         <textarea cols='50' className='create_quiz_content' name='quizContent' onChange={this.handleOnChange} placeholder='Content' rows='5' required value={this.state.quizContent} />
-        {this.state.questions.map((question) => {
+        {this.state.questions.map((question, index) => {
           return (
-            <div key={question.num}>
-              <textarea cols='50' className='question' name={question.num} placeholder={`Question ${question.num + 1}`} onChange={this.questionChange} rows='5' required value={question.question} />
+            <div key={index}>
+              <textarea cols='50' className='question' name={index} placeholder={`Question ${index + 1}`} onChange={this.questionChange} rows='5' required value={question} />
               <fieldset className='question_fieldset'>
                 <div>
                   <input type='radio' />
@@ -85,17 +78,17 @@ class CreateQuiz extends Component {
                   <input className='question_choices' placeholder='Choice 2' type='text' />
                 </div>
                 <div>
-                  <input disabled={this.state.choices[question.num]['3'][1]} type='radio' />
-                  <input className='question_choices' disabled={this.state.choices[question.num]['3'][1]} placeholder='Choice 3' type='text' />
-                  <Button name={question.num} id='3' className='enable_answer' onClick={this.enableOrDisable}>Enable Answer</Button>
+                  <input disabled={this.state.choices[index]['3'][1]} type='radio' />
+                  <input className='question_choices' disabled={this.state.choices[index]['3'][1]} placeholder='Choice 3' type='text' />
+                  <Button name={index} id='3' className='enable_answer' onClick={this.enableOrDisable}>Enable Answer</Button>
                 </div>
                 <div>
-                  <input disabled={this.state.choices[question.num]['4'][1]} type='radio' />
-                  <input className='question_choices' disabled={this.state.choices[question.num]['4'][1]} placeholder='Choice 4' type='text' />
-                  <Button name={question.num} id='4' className='enable_answer' onClick={this.enableOrDisable}>Enable Answer</Button>
+                  <input disabled={this.state.choices[index]['4'][1]} type='radio' />
+                  <input className='question_choices' disabled={this.state.choices[index]['4'][1]} placeholder='Choice 4' type='text' />
+                  <Button name={index} id='4' className='enable_answer' onClick={this.enableOrDisable}>Enable Answer</Button>
                 </div>
               </fieldset>
-              <Button color='danger' className='delete_question' name={question.num} onClick={this.deleteQuestion}>Delete Question</Button>
+              <Button color='danger' className='delete_question' name={index} onClick={this.deleteQuestion}>Delete Question</Button>
             </div>
           )
         })}
