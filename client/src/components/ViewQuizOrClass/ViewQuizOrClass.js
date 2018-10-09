0e46676c90a071ff7eecd8410b0e049e880c5514
bbenefield89 <bsquared18@gmail.com>
2018-10-09 16:39:31 -0400

    ADD 'maxWidth' ATTRIBUTE/PROPTYPE
    - client/src/components/ViewQuizOrClass/ViewQuizOrClass.js

--- client/src/components/ViewQuizOrClass/ViewQuizOrClass.js
+++ client/src/components/ViewQuizOrClass/ViewQuizOrClass.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { func } from 'prop-types'
+import { func, string } from 'prop-types'
 
 import { Cards } from '../Quizzes/styled'
 
 const ViewQuizOrClass = props => {
   return (
-    <Cards>
+    <Cards maxWidth={props.maxWidth}>
       {props.render(props)}
     </Cards>
   )
 }
 
 ViewQuizOrClass.propTypes = {
-  render: func
+  render: func,
+  maxWidth: string
 }
 
 export default ViewQuizOrClass
