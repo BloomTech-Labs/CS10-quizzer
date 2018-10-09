fcf10699b726d125a90b34ba8987ab2f6dbb2b4c
bbenefield89 <bsquared18@gmail.com>
2018-10-09 16:35:09 -0400

    FIX HTML HIERARCHY FOR READABILITY
    - client/src/components/Classes/Classes.js
      - pass a "maxWidth" prop to <ViewQuizOrClass />
      - Add <Row /> and <Col /> FOR STYLING

--- client/src/components/Classes/Classes.js
+++ client/src/components/Classes/Classes.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
-import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+import { Row, Col } from 'reactstrap'
+
 import ViewQuizOrClass from '../ViewQuizOrClass/ViewQuizOrClass'
 import ClassList from './ClassList'
+import NewClassCard from './NewClassCard'
+
 import './Classes.css'
 
 import Styles from '../RocketStyles'
@@ -41,6 +45,7 @@ class Classes extends Component {
     return (
       <ViewQuizOrClass
         key={classSet.ClassID}
+        maxWidth='100%'
         render={() => (
           <ClassList
             classSet={classSet}
@@ -53,18 +58,28 @@ class Classes extends Component {
   render () {
     return (
       <Styles>
-        <h1>Classes</h1>
+        <Row>
+          <Col
+            className='text-center d-flex flex-column flex-wrap align-items-center px-4 mb-5'
+          >
+            <h1 className='mb-3'>Classes</h1>
 
-        <Query query={GET_CLASSES_INFORMATION} variables={{ token: localStorage.getItem('token') }}>
-          {({ loading, data, error }) => {
-            if (loading) return <h1>Loading...</h1>
-            if (error) return <h1>{error.message}</h1>
+            <NewClassCard />
+          </Col>
+        </Row>
 
-            if (data) {
-              return this.renderClassComponent(data)
-            }
-          }}
-        </Query>
+        <Row className='h-100 m-0 px-3'>
+          <Query query={GET_CLASSES_INFORMATION} variables={{ token: localStorage.getItem('token') }}>
+            {({ loading, data, error }) => {
+              if (loading) return <h1>Loading...</h1>
+              if (error) return <h1>{error.message}</h1>
+
+              if (data) {
+                return (this.renderClassComponent(data))
+              }
+            }}
+          </Query>
+        </Row>
       </Styles>
     )
   }
