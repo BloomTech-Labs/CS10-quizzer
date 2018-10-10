03c5f1dc73ee3bf477885c74ef3dbd69a5bb0b97
bbenefield89 <bsquared18@gmail.com>
2018-10-09 23:15:46 -0400

    ADD MEDIA QUERIES
    - client/src/components/Quizzes/styled.js
    - client/src/components/Quizzes/Quizzes.js

--- client/src/components/Quizzes/styled.js
+++ client/src/components/Quizzes/styled.js
@@ -5,10 +5,22 @@ export const AddQuizContainer = styled.div`
   flex-wrap: wrap;
   justify-content: center;
 
-  @media (min-width: 930px) {
-    justify-content: space-around;
+  @media (min-width: 794px) {
+    justify-content: space-between
+  }
+
+  @media (min-width: 818px) {
+    justify-content: center;
+  }
+  
+  @media (min-width: 994px) {
+    justify-content: center;
     padding-left: .5rem;
     padding-right: .5rem;
+  }
+
+  @media (min-width: 1010px) {
+    justify-content: flex-start
   }
 `
 
@@ -16,4 +28,8 @@ export const Cards = styled.div`
   margin-bottom: 3rem;
   width: 100%;
   max-width: ${({ maxWidth = '350px' }) => maxWidth};
+
+  @media (min-width: 730px) {
+    margin-right: 2rem;
+  }
 `
