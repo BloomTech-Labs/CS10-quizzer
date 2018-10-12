c00fb7fbefdf21157b7087de56fc573d8145387b
Mephestys <mephestys@gmail.com>
2018-10-12 15:33:28 -0600

    Fit legible text into class cards, remove link highlights/underline

--- client/src/components/Classes/ClassCard.js
+++ client/src/components/Classes/ClassCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardTitle, CardBody, CardText } from 'reactstrap'
+import { Card, CardTitle, CardBody } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
@@ -14,12 +14,14 @@ function ClassCard (props) {
 
   return (
     <Card className='quiz_card'>
-      <Link to={{ pathname: '/rocket/classes/editclass/', state: { classItem: props.classroom } }}>
+      <Link className='class_card_link' to={{ pathname: '/rocket/classes/editclass/', state: { classItem: props.classroom } }}>
         <CardBody>
           <CardTitle className='quiz_card_title'>{props.classroom.ClassName}</CardTitle>
-          <CardText className='class_card_text'>Students: {props.classroom.studentSet.length}</CardText>
-          <CardText className='class_card_text'>Average Grade: { isNaN(averageGrade) ? 'No grades yet' : `${averageGrade}%` }</CardText>
-          <CardText className='class_card_text'>Quizzes: {props.classroom.quizSet.length}</CardText>
+          <span className='class_card_text'>Students: {props.classroom.studentSet.length}</span>
+          <br />
+          <span className='class_card_text'>Average Grade: { isNaN(averageGrade) ? 'No grades yet' : `${averageGrade}%` }</span>
+          <br />
+          <span className='class_card_text'>Quizzes: {props.classroom.quizSet.length}</span>
         </CardBody>
       </Link>
     </Card>
