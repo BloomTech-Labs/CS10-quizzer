7cf06928018874377deb168a922d5bb27578afad
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-10-12 12:26:10 -0400

    Modified models.py and styled.js in RocketList.

--- quizzes/models.py
+++ quizzes/models.py
@@ -60,7 +60,7 @@ class Question(models.Model):
 class Choice(models.Model):
     ChoiceID      = models.UUIDField(primary_key=True, default = uuid4, editable = False)
     QuestionID    = models.ForeignKey('Question', on_delete = models.CASCADE)
-    ChoiceText    = models.TextField(blank = False)
+    ChoiceText    = models.TextField()
     isCorrect     = models.BooleanField()
     created_at    = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
